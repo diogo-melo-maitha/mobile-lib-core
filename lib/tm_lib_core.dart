88fe13a54cde2be29624eb57a8f9cc8347269f67
RodrigoCavreti-Trademaster <rodrigo.cavreti@trademaster.com.br>
2021-05-12 17:00:34 -0300

    exposing environment config class

--- lib/tm_lib_core.dart
+++ lib/tm_lib_core.dart
@@ -2,3 +2,4 @@ library tm_lib_core;
 
 export 'package:tm_lib_core/security/cryptography_utils.dart';
 export 'package:tm_lib_core/security/security_repository.dart';
+export 'package:tm_lib_core/configuration/environment_config.dart';
