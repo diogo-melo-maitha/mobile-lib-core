45f60e274c7329c8ef6d469337de0165f19ae2b1
RodrigoCavreti-Trademaster <rodrigo.cavreti@trademaster.com.br>
2021-05-17 17:19:07 -0300

    initial implementation of permissions utils added

---
+++ lib/utils/permission_utils.dart
@@ -0,0 +1,14 @@
+import 'package:permission_handler/permission_handler.dart';
+
+class PermissionUtils {
+  static Future<bool> requestStoragePermission() async {
+    return await Permission.storage.request() == PermissionStatus.granted;
+  }
+
+  static Future<bool> requestCameraPermission() async {
+    return await Permission.camera.request() == PermissionStatus.granted;
+  }
+
+  /// We will add in the future methods to help show errors ou warnings about permissions, as soon as the designs are available it will be added here
+
+}
