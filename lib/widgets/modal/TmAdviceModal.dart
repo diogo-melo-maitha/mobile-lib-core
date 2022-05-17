116ab9601a391cc45d5a9978e0cb9291022e610b
VitorMarceloBispo <86015172+vitor-bispo-tm@users.noreply.github.com>
2022-05-17 14:13:37 -0300

    TmAdviceModal widget finished, needs unit test

--- lib/widgets/modal/TmAdviceModal.dart
+++ lib/widgets/modal/TmAdviceModal.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../configuration/tm_colors.dart';
@@ -7,17 +6,47 @@ import '../../configuration/tm_fonts.dart';
 class TmAdviceModal extends StatelessWidget {
   final String title;
   final String clickableText;
+  final Function() onTap;
 
   const TmAdviceModal({
     Key? key,
-    required this.clickableText,
     required this.title,
+    required this.clickableText,
+    required this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final TextStyle fontStyle = TmFonts.bold13.merge(const TextStyle(color: TmColors.white));
+    final TextStyle textStyle = TmFonts.semiBold12.merge(
+      const TextStyle(color: TmColors.turquoise),
+    );
 
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 14),
+      child: Container(
+        decoration: BoxDecoration(color: TmColors.adviceYellow, borderRadius: BorderRadius.circular(5)),
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: textStyle,
+            ),
+            InkWell(
+              onTap: onTap,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: Text(
+                  clickableText,
+                  style: textStyle.merge(const TextStyle(decoration: TextDecoration.underline)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
